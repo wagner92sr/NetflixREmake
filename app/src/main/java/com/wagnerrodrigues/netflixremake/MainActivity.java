03832e9007c45895107c17c28c86e5c9be6fea60
Wagner Rodrigues <wagnersr92@gmail.com>
2019-09-10 00:29:19 -0300

    Criação de lista

--- app/src/main/java/com/wagnerrodrigues/netflixremake/MainActivity.java
+++ app/src/main/java/com/wagnerrodrigues/netflixremake/MainActivity.java
@@ -2,13 +2,22 @@ package com.wagnerrodrigues.netflixremake;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wagnerrodrigues.netflixremake.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,31 +26,53 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view_main);
 
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0;i < 30; i++ ){
+            Movie movie = new Movie();
+            movie.setCoverUrl("abc" + i);
+            movies.add(movie);
+        }
+
+        mainAdapter  = new MainAdapter(movies);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+        recyclerView.setAdapter(mainAdapter);
+
     }
 
     private static class MovieHolder extends RecyclerView.ViewHolder{
 
+        private final TextView textViewUrl;
+
         public MovieHolder(@NonNull View itemView) {
             super(itemView);
+            textViewUrl = itemView.findViewById(R.id.textView_url);
         }
     }
 
     private class MainAdapter extends RecyclerView.Adapter<MovieHolder>{
 
+        private final List<Movie> movies;
+
+        private MainAdapter(List<Movie> movies){
+            this.movies = movies;
+        }
+
         @NonNull
         @Override
         public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            return new MovieHolder(getLayoutInflater().inflate(R.layout.movie_item, parent,false));
         }
 
         @Override
         public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
+            Movie movie = movies.get(position);
+            holder.textViewUrl.setText(movie.getCoverUrl());
 
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return movies.size();
         }
     }
 
