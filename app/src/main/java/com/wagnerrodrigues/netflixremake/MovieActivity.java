581e92365d935b775b2ffcce0dc81b1c077e7990
Wagner Rodrigues <wagnersr92@gmail.com>
2019-09-14 20:51:13 -0300

    Criação da MovieActivity para listar os detalhes do filme e toolbar com seta para voltar à tela anterior.

---
+++ app/src/main/java/com/wagnerrodrigues/netflixremake/MovieActivity.java
@@ -0,0 +1,26 @@
+package com.wagnerrodrigues.netflixremake;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if( getSupportActionBar() != null){
+            getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+        }
+
+
+    }
+}
