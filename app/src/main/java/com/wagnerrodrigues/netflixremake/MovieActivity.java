b418b35f4eea4b2b9af31024b67112879ed57a84
Wagner Rodrigues <wagnersr92@gmail.com>
2019-09-15 01:03:04 -0300

    Adição de sombras na imagem do filme

--- app/src/main/java/com/wagnerrodrigues/netflixremake/MovieActivity.java
+++ app/src/main/java/com/wagnerrodrigues/netflixremake/MovieActivity.java
@@ -2,8 +2,12 @@ package com.wagnerrodrigues.netflixremake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 
 public class MovieActivity extends AppCompatActivity {
@@ -21,6 +25,14 @@ public class MovieActivity extends AppCompatActivity {
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
         }
 
+        LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this,R.drawable.shadows);
 
+        if(drawable != null){
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
+
+            drawable.setDrawableByLayerId(R.id.cover_drawable, movieCover);
+
+            ((ImageView) findViewById(R.id.imageView_cover)).setImageDrawable(drawable);
+        }
     }
 }
