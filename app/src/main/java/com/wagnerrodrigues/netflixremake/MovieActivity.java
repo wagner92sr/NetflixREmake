4f08d0603a45b5b7792684c483f186388fb5681b
Wagner Rodrigues <wagnersr92@gmail.com>
2019-09-26 23:54:36 -0300

    Finalizando o layout de detalhes do filme

--- app/src/main/java/com/wagnerrodrigues/netflixremake/MovieActivity.java
+++ app/src/main/java/com/wagnerrodrigues/netflixremake/MovieActivity.java
@@ -8,14 +8,23 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MovieActivity extends AppCompatActivity {
+
+    private TextView txtTitle;
+    private TextView txtDesc;
+    private TextView txtCast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        txtTitle = findViewById(R.id.text_view_title);
+        txtDesc = findViewById(R.id.text_view_desc);
+        txtCast = findViewById(R.id.text_view_cast);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -23,6 +32,7 @@ public class MovieActivity extends AppCompatActivity {
         if( getSupportActionBar() != null){
             getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+            getSupportActionBar().setTitle(null);
         }
 
         LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this,R.drawable.shadows);
@@ -34,5 +44,9 @@ public class MovieActivity extends AppCompatActivity {
 
             ((ImageView) findViewById(R.id.imageView_cover)).setImageDrawable(drawable);
         }
+
+        txtTitle.setText("Batman Begins");
+        txtDesc.setText("O jovem Bruce Wayne tralalá blá blá blá cobate o crime na cidade de Gothan");
+        txtCast.setText(getString(R.string.cast, "Wagner" + ",Teste" + ",teste" + ",teste"));
     }
 }
